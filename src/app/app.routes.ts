7451659a76100a9eb418c434f21abb746b24f77e
Johana catalina rios torres <jcriostorres@gmail.com>
2020-11-11 20:11:55 -0500

    se agregaron las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,15 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { RutasComponent } from './components/rutas/rutas.component';
 
 // arreglo de rutas
 const APP_ROUTES: Routes =[
     { path: 'home', component: HomeComponent},
-    { path: 'about', component: HomeComponent},
+    { path: 'about', component: AboutComponent},
+    { path: 'rutas', component: RutasComponent},
     { path: '**', pathMatch: 'full', redirectTo: 'home' }
 
 ];
 
-export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, {useHash:true});
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
